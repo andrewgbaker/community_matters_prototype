25d06cd4b3ede3cbe4ec0d1a0369259a16060b67
Andrew Baker <agb.baker@gmail.com>
2013-04-04 10:43:17 -0600

    fix scrolling to specific story

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
 var $container = $('#container')
 
+// SET UP CENTERED MASONRY FUNCTION
+					
+	  $.Isotope.prototype._getCenteredMasonryColumns = function() {
+    this.width = this.element.width();
+    
+    var parentWidth = this.element.parent().width();
+    
+                  // i.e. options.masonry && options.masonry.columnWidth
+    var colW = this.options.masonry && this.options.masonry.columnWidth ||
+                  // or use the size of the first item
+                  this.$filteredAtoms.outerWidth(true) ||
+                  // if there's no items, use size of container
+                  parentWidth;
+    
+    var cols = Math.floor( parentWidth / colW );
+    cols = Math.max( cols, 1 );
+
+    // i.e. this.masonry.cols = ....
+    this.masonry.cols = cols;
+    // i.e. this.masonry.columnWidth = ...
+    this.masonry.columnWidth = colW;
+  };
+  
+  $.Isotope.prototype._masonryReset = function() {
+    // layout-specific props
+    this.masonry = {};
+    // FIXME shouldn't have to call this again
+    this._getCenteredMasonryColumns();
+    var i = this.masonry.cols;
+    this.masonry.colYs = [];
+    while (i--) {
+      this.masonry.colYs.push( 0 );
+    }
+  };
+
+  $.Isotope.prototype._masonryResizeChanged = function() {
+    var prevColCount = this.masonry.cols;
+    // get updated colCount
+    this._getCenteredMasonryColumns();
+    return ( this.masonry.cols !== prevColCount );
+  };
+  
+  $.Isotope.prototype._masonryGetContainerSize = function() {
+    var unusedCols = 0,
+        i = this.masonry.cols;
+    // count unused columns
+    while ( --i ) {
+      if ( this.masonry.colYs[i] !== 0 ) {
+        break;
+      }
+      unusedCols++;
+    }
+    
+    return {
+          height : Math.max.apply( Math, this.masonry.colYs ),
+          // fit container to columns that have been used;
+          width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
+        };
+  };
+
 // INITIATE JQUERY BBQ HASH HISTORY AND ISOTOPE
   
  var history = {
@@ -102,9 +162,11 @@ var $container = $('#container')
 		            that.find( '.story-content' ).children( ':visible' ).toggleClass( 'expanded_item' );
 			        setTimeout(function(){ 
 				         $container.isotope('reLayout', function(){
-					        $('html,body').animate({scrollTop:$('.story_title').offset().top -100}, 500);
+				         	var closestTitle = that.find('.story_title');
+				         
+					        $('html,body').animate({scrollTop:$(closestTitle).offset().top - 95}, 500);
 					     });
-					}, 600 );
+					}, 500 );
 
 		          }); 
 		 
@@ -125,68 +187,8 @@ var $container = $('#container')
 	  // BEGIN ISOTOPE JS
 	  
 	  gridSetup:function(){
-	  
-				  // SET UP CENTERED MASONRY FUNCTION
-			
-			  $.Isotope.prototype._getCenteredMasonryColumns = function() {
-			    this.width = this.element.width();
-			    
-			    var parentWidth = this.element.parent().width();
-			    
-			                  // i.e. options.masonry && options.masonry.columnWidth
-			    var colW = this.options.masonry && this.options.masonry.columnWidth ||
-			                  // or use the size of the first item
-			                  this.$filteredAtoms.outerWidth(true) ||
-			                  // if there's no items, use size of container
-			                  parentWidth;
-			    
-			    var cols = Math.floor( parentWidth / colW );
-			    cols = Math.max( cols, 1 );
-			
-			    // i.e. this.masonry.cols = ....
-			    this.masonry.cols = cols;
-			    // i.e. this.masonry.columnWidth = ...
-			    this.masonry.columnWidth = colW;
-			  };
-			  
-			  $.Isotope.prototype._masonryReset = function() {
-			    // layout-specific props
-			    this.masonry = {};
-			    // FIXME shouldn't have to call this again
-			    this._getCenteredMasonryColumns();
-			    var i = this.masonry.cols;
-			    this.masonry.colYs = [];
-			    while (i--) {
-			      this.masonry.colYs.push( 0 );
-			    }
-			  };
-			
-			  $.Isotope.prototype._masonryResizeChanged = function() {
-			    var prevColCount = this.masonry.cols;
-			    // get updated colCount
-			    this._getCenteredMasonryColumns();
-			    return ( this.masonry.cols !== prevColCount );
-			  };
-			  
-			  $.Isotope.prototype._masonryGetContainerSize = function() {
-			    var unusedCols = 0,
-			        i = this.masonry.cols;
-			    // count unused columns
-			    while ( --i ) {
-			      if ( this.masonry.colYs[i] !== 0 ) {
-			        break;
-			      }
-			      unusedCols++;
-			    }
-			    
-			    return {
-			          height : Math.max.apply( Math, this.masonry.colYs ),
-			          // fit container to columns that have been used;
-			          width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
-			        };
-			  };
-	  
-	  
+
+
 	          // object that will keep track of options
 	          isotopeOptions = {},
 	          // defaults, used if not explicitly set in hash
@@ -196,7 +198,7 @@ var $container = $('#container')
 	            sortAscending: true,
 	            layoutMode: 'masonry'
 	          };
-			        
+
 	      var setupOptions = $.extend( {}, defaultOptions, {
 	        itemSelector : '.element',
 	        masonry: {
@@ -208,15 +210,15 @@ var $container = $('#container')
 	          }
 	        },
 	        sortBy: 'selected' 
-	        
+
 	      });	
-	      
+
 	      // set up Isotope
 	      $container.isotope( setupOptions );
-	  
+
 	      var $optionSets = $('#options').find('.option-set'),
 	          isOptionLinkClicked = false;
-	  
+
 	      // switches selected class on buttons
 	      function changeSelectedLink( $elem ) {
 	        // remove selected class on previous item
@@ -224,8 +226,8 @@ var $container = $('#container')
 	        // set selected class on new item
 	        $elem.addClass('selected');
 	      }
-	  
-	  
+
+
 	      $optionSets.find('a').click(function(){
 	        var $this = $(this);
 	        // don't proceed if already selected
@@ -245,9 +247,9 @@ var $container = $('#container')
 	        isOptionLinkClicked = true;
 	        return false;
 	      });
-	
+
 	      var hashChanged = false;
-	
+
 	      $(window).bind( 'hashchange', function( event ){
 	        // get options object from hash
 	        var hashOptions = window.location.hash ? $.deparam.fragment( window.location.hash, true ) : {},
@@ -259,7 +261,7 @@ var $container = $('#container')
 	        $container.isotope( options );
 	        // save options
 	        isotopeOptions = hashOptions;
-	    
+
 	        // if option link was not clicked
 	        // then we'll need to update selected links
 	        if ( !isOptionLinkClicked ) {
@@ -276,13 +278,13 @@ var $container = $('#container')
 	            changeSelectedLink( $selectedLink );
 	          }
 	        }
-	    
+
 	        isOptionLinkClicked = false;
 	        hashChanged = true;
 	      })
 	        // trigger hashchange to capture any hash data on init
 	        .trigger('hashchange');
-	        
+
 	  },
 	  
 	  // END GRID SETUP   
@@ -307,23 +309,6 @@ var $container = $('#container')
 			    });
 	
 			    */
-			
-		openStory:function(){
-			    
-			    
-			    $(".element").on("click", ".vid_link", function(){
-		      
-			            $(this).closest('.element').toggleClass('story_width');
-			            $(this).find( ".story-content" ).children( ':visible' ).hide();
-				        $(this).closest(".story-default").show();
-		   				
-		   				setTimeout(function(){ 
-				         $container.isotope('reLayout');
-						  }, 500 ); 
-						  
-						  		        
-			   });
-		},
 		
 		closeStory:function(){
 			    
@@ -347,14 +332,46 @@ var $container = $('#container')
 			   });
 	        
 	    },
-	  
-	  aboutGrid:function(){
-	  
-	  		var $mapContainer = $('#map_container')
+	    
+	    openStory:function(){
 			    
-			    $mapContainer.isotope({
-			      itemSelector: '.map_item'
-     			});
+			    
+			    $(".element").on("click", ".vid_link", function(){
+		      
+			            $(this).closest('.element').toggleClass('story_width');
+			            $(this).find( ".story-content" ).children( ':visible' ).hide();
+				        $(this).closest(".story-default").show();
+		   				
+		   				setTimeout(function(){ 
+				         $container.isotope('reLayout');
+						  }, 500 ); 
+						  
+						  		        
+			   });
+		},
+	  
+	  aboutGrid:function(){	
+			      
+			      // object that will keep track of options
+			          isotopeOptions = {},
+			          // defaults, used if not explicitly set in hash
+			          defaultOptions = {
+			            filter: '*',
+			            sortBy: 'original-order',
+			            sortAscending: true,
+			            layoutMode: 'masonry'
+			          };
+		
+			      var setupOptions = $.extend( {}, defaultOptions, {
+			        itemSelector : '.map_item',
+			        masonry: {
+					    columnWidth: 300
+					  }
+		
+			      });	
+		
+			      // set up Isotope
+			      $container.isotope( setupOptions );
 			      
 			}
 	        
